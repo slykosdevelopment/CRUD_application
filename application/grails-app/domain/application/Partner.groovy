261bcf49dde6482f82cd0fc39e062583b7097373
slykosdevelopment <slykos.development@gmail.com>
2024-03-26 00:23:27 +0100

    created partner class

---
+++ application/grails-app/domain/application/Partner.groovy
@@ -0,0 +1,12 @@
+package application
+
+class Partner {
+	long id 
+	String companyName 
+	String ref 
+	Locale locale
+	Date expires 
+
+    static constraints = {
+    }
+}
---
+++ application/src/test/groovy/application/PartnerSpec.groovy
@@ -0,0 +1,18 @@
+package application
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class PartnerSpec extends Specification implements DomainUnitTest<Partner> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
